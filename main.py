f9ee74035a418b672da42f5ac3803f2c778e6675
Theo <theomhalpern@gmail.com>
2023-03-13 04:44:46 +0000

    added words

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from replit import db as users
 import schemas
 import crud
 
-from .genImage import genImage
+import asyncio
+
+from genImage import genImage
 
 
 from typing import Union, Optional
 
-from fastapi import FastAPI, Path, Query, HTTPException, status
+from fastapi import FastAPI, Path, Query, HTTPException, status, BackgroundTasks
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -48,8 +50,14 @@ def get_users():
     return users
 
 
-# function for updating points, function for creating user, function for getting points, function for generating and outputting leaderboard
-
+# need to make function for generating and outputting leaderboard
+@app.get("/generate-leaderboard")
+def generate_leaderboard():
+  leaderboard = sorted(users.items(),key=lambda x:x[1], reverse = True)
+  
+  
+  return(dict(leaderboard))
+  
 @app.get("/create-user/{username}")
 def create_item(username:str, points: int):
     if username in users:
@@ -66,13 +74,49 @@ def update_user(username:str, points: int):
     users[username] += points
     return users[username]
 
+images = []
+
+
 @app.get("/get-image")
-def get_image():
-  Image = {
-    
-  }
-  return genImage()
+def get_image(background_tasks: BackgroundTasks):
+  #  return(usedWords, allWords, response["data"][0]["url"])
+  background_tasks.add_task(gen_image)
+  if(len(images)==0):
+    print("number of images in stack: "+str(len(images)))
+    result = genImage()
+    print("new image loaded")
+    ai_result = {
+      "usedWords": result[0],
+      "allWords": result[1],
+      "url": result[2]
+    }
+    return images.append(ai_result)
   
+    
+  return images.pop()
+  
+async def gen_image():
+  print("number of images in stack: "+str(len(images)))
+  result = genImage()
+  print("new image loaded")
+  ai_result = {
+    "usedWords": result[0],
+    "allWords": result[1],
+    "url": result[2]
+  }
+  images.append(ai_result)
+
+async def createFirstImages():
+    # Schedule three calls *concurrently*:
+    L = await asyncio.gather(
+        gen_image(),
+      gen_image(),gen_image()
+    )
+    print(L)
+
+# TODO: check for stale images on user log in
+asyncio.run(createFirstImages())
+
 
 @app.delete("/delete-user")
 def delete_user(username :str = Query(..., description = "delete the user with the username")):
